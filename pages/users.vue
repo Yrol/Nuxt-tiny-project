5a93c887146171cbe5f0727d9c685cc856772432
Yrol <yrollgayanth@yahoo.com>
2020-06-09 22:28:03 +1000

    define and inherit child elements

---
+++ pages/users.vue
@@ -0,0 +1,10 @@
+<!--
+This component will be inherited by the "users" page and all its child pages
+Using "nuxt-child" property to create the relationship
+ -->
+<template>
+  <div>
+    <p> You'll see me in all users related pages </p>
+    <nuxt-child />
+  </div>
+</template>
