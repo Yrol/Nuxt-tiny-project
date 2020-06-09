550624013b393a86cbef40d8f6f82683dfa1f3ba
Yrol <yrollgayanth@yahoo.com>
2020-06-09 21:47:51 +1000

    first commit

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,18 @@
+<template>
+<h1>The user ID is {{$route.params.id}}</h1>
+</template>
+
+<script>
+export default {
+
+  /*
+    Nuxt validate method
+    Using the Regex to validate the param to be a number
+    Using the "context" object to access properties such as "params" in this case
+  */
+  //validate({params}) // can also destructure instead of context below
+  validate(context) {
+    return /^\d+$/.test(context.params.id)
+  }
+}
+</script>
---
+++ pages/users/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Main user page</h1>
+    <input type="text" v-model="userID" />
+
+    <button @click="loadUser">Load user</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      userID: ''
+    }
+  },
+
+  methods: {
+    loadUser () {
+      this.$router.push(`/users/${this.userID}`)
+    }
+  }
+}
+</script>
